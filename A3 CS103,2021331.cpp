6771cc9bd8f9ffe0ef8b2f7576b800a4c48b2018
ahmad9022032 <94382791+ahmad9022032@users.noreply.github.com>
2021-11-15 21:34:23 +0500

    Add files via upload

---
+++ A3 CS103,2021331.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str = "ahmad";
+
+	reverse(str.begin(), str.end());
+
+	 cout << str;
+
+	return 0;
+
+}
